76fdcf360a581ad2bea9b6004b1af23d4fa48951
Eiyeron Fulmincendii <eiyeron@gmail.com>
2015-08-14 11:35:35 +0200

    Added code.
    Yay code!

---
+++ test/tests_parser.py
@@ -0,0 +1,64 @@
+import unittest
+import os
+import json
+import struct
+
+from wrpg.piaf import parser
+from wrpg.piaf.parser import (
+    ParserError,
+    ParserMagicHeaderError,
+    ParserChecksumError,
+    ParserDatasizeError
+)
+
+sample_directory='test/samples/'
+expected_suffix='_expected'
+valid_folder='valid'
+
+
+
+class TestParser (unittest.TestCase):
+    def test_valid_samples(self):
+        #print('Valid samples testing.\n')
+        valid_folder_path = os.path.join(sample_directory, valid_folder)
+        valid_expected_folder = os.path.join(sample_directory, valid_folder+expected_suffix)
+        list_files = [f for f in os.listdir(valid_folder_path)
+                      if os.path.isfile(os.path.join(valid_folder_path, f))
+                      and f.endswith(".wrf")]
+
+        for f in list_files:
+            path_sample = os.path.join(sample_directory, valid_folder, f)
+            path_expected = os.path.join(sample_directory, valid_folder+expected_suffix,
+                                         f+'.json')
+            #print('{} => {}'.format(path_sample, path_expected))
+            with open(path_sample, 'br') as sample, open(path_expected, 'r') as expected:
+                archive = parser.parse_archive(sample.read())
+                self.assertEqual(archive, json.load(expected))
+
+    def test_invalid_samples(self):
+        ### Header
+        # Emtpy argument
+        with self.assertRaises(struct.error):
+            parser.parse_archive(b'')
+
+        # Header too small
+        with self.assertRaises(struct.error):
+            parser.parse_archive(b'The Game')
+
+        # Bad header
+        with self.assertRaises(ParserMagicHeaderError):
+            parser.parse_archive(b'ERRHEADER\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0')
+
+        # Bad checksum
+        with self.assertRaises(ParserChecksumError):
+            # Wrong header checksum
+            parser.parse_archive(b'WRPGPIAF\x0D\x0E\x0A\x0D\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0')
+        with self.assertRaises(ParserChecksumError):
+            # Wrong filetable checksum
+            parser.parse_archive(b'WRPGPIAF\0\0\0\0\x0D\x0E\x0A\x0D\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0')
+
+        # Wrong data size
+        with self.assertRaises(ParserDatasizeError):
+            # Bad size because wrong file number
+            parser.parse_archive(b'WRPGPIAF\xc6\x0f\x16#\0\0\0\0\0\0\0\0\r\x0e\n\r\0\0\0\0\0\0\0\0')
+        ### Files
---
+++ wrpg/piaf/parser.py
@@ -0,0 +1,87 @@
+import struct
+import string
+import unittest
+import binascii
+
+class ParserError(Exception):
+    pass
+
+class ParserMagicHeaderError(ParserError):
+    pass
+
+class ParserChecksumError(ParserError):
+    pass
+
+class ParserDatasizeError(ParserError):
+    pass
+
+def archive_structure():
+    return (
+        ">" # Big Endian
+        "8s" # Magic Header
+        "I" # Header Checksum
+        "I" # File table Checksum
+        # Offset : 24 (For checksum)
+        "I" # Version
+        "I" # NB Files
+        "I" # Data Size
+        "4x" # Padding
+    )
+
+def file_entry_structure():
+    return (
+        ">" # Big Endian
+        "I" # File Type
+        "I" # Compression type
+        "I" # File Size
+        "I" # Data Offset
+    )
+
+def check_archive(buffer, archive, checksum_header, checksum_filetable):
+    def check_header(buffer):
+        return binascii.crc32(buffer[16:32], 0) & 0xffffffff
+
+    def check_filetable(buffer, archive):
+        filetable_end = 32+24*archive["nb_files"]
+        return binascii.crc32(buffer[32:filetable_end]) & 0xffffffff
+
+    file_checksum_header = check_header(buffer)
+    if file_checksum_header != checksum_header:
+        raise ParserChecksumError('Bad Header Checksum : {} != {}'.format(file_checksum_header, checksum_header))
+
+    file_checksum_filetable = check_filetable(buffer, archive)
+
+    if file_checksum_filetable != checksum_filetable:
+        raise ParserChecksumError('Bad Filetable Checksum : {} != {}'.format(file_checksum_filetable, check_filetable))
+
+def check_data_size(buffer, archive):
+    if len(buffer) != archive["data_size"] + 32 + 24*archive["nb_files"]:
+        raise ParserDatasizeError('Bad Data Size')
+
+def parse_header(buffer):
+    header = buffer[:32]
+    parsed_header = struct.unpack(archive_structure(), header)
+    magic_header = parsed_header[0].decode('utf-8')
+    archive = {
+    "version": parsed_header[3],
+    "nb_files": parsed_header[4],
+    "data_size": parsed_header[5]
+    }
+
+    if magic_header != 'WRPGPIAF':
+        raise ParserMagicHeaderError('Bad Magic Header')
+
+    check_archive(buffer, archive, parsed_header[1], parsed_header[2])
+    check_data_size(buffer, archive)
+    return archive
+
+def parse_filetable(buffer, archive):
+    result = []
+    for i in range(0, archive["nb_files"]):
+        result.append(struct.unpack(file_entry_structure(), archive[32+24*i:32+24*(i+1)]))
+    return result
+
+def parse_archive(buffer):
+    archive = parse_header(buffer)
+    archive["file_entries"] = parse_filetable(buffer, archive)
+    return archive
