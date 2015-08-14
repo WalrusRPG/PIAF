d5a4204dda94b47d06ff111d5f8366ba3afafc13
Eiyeron Fulmincendii <eiyeron@gmail.com>
2015-08-14 11:55:12 +0200

    Exported structures to a common file.

---
+++ wrpg/piaf/common.py
@@ -0,0 +1,22 @@
+
+def archive_structure():
+    return (
+        ">" # Big Endian
+        "8s" # Magic Header
+        "I" # Header Checksum
+        "I" # File table Checksum
+        # Offset : 24 (For checksum)
+        "I" # Version
+        "I" # NB Files
+        "I" # Data Size
+        "4x" # Padding
+    )
+
+def file_entry_structure():
+    return (
+        ">" # Big Endian
+        "I" # File Type
+        "I" # Compression type
+        "I" # File Size
+        "I" # Data Offset
+    )
--- wrpg/piaf/parser.py
+++ wrpg/piaf/parser.py
@@ -2,6 +2,7 @@ import struct
 import string
 import unittest
 import binascii
+from .common import (archive_structure, file_entry_structure)
 
 class ParserError(Exception):
     pass
@@ -15,27 +16,6 @@ class ParserChecksumError(ParserError):
 class ParserDatasizeError(ParserError):
     pass
 
-def archive_structure():
-    return (
-        ">" # Big Endian
-        "8s" # Magic Header
-        "I" # Header Checksum
-        "I" # File table Checksum
-        # Offset : 24 (For checksum)
-        "I" # Version
-        "I" # NB Files
-        "I" # Data Size
-        "4x" # Padding
-    )
-
-def file_entry_structure():
-    return (
-        ">" # Big Endian
-        "I" # File Type
-        "I" # Compression type
-        "I" # File Size
-        "I" # Data Offset
-    )
 
 def check_archive(buffer, archive, checksum_header, checksum_filetable):
     def check_header(buffer):
